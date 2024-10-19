3250113783a30709626ec77c774467808b95662f
DanielCondia <daniel.condia@gmail.com>
2024-10-19 18:03:34 -0500

    :rocket: :tada: Se inicia proyecto. Se hace configuracion de composer Ratchet con socket

--- src/SocketMap.php
+++ src/SocketMap.php
@@ -1,8 +1,39 @@
 <?php
 
-namespace src;
-
-class Map
+namespace MyMap;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+class SocketMap implements MessageComponentInterface
 {
 
+    protected $clientes;
+
+    public function __construct()
+    {
+        $this->clientes = new \SplObjectStorage;
+        echo "Iniciando Servidor de Sockets...\n";
+    }
+
+    function onOpen(ConnectionInterface $conn)
+    {
+        $this->clientes->attach($conn); // Almacena la nueva conexión para interactuar con el cliente más tarde
+        echo "Nueva Conexión => ({$conn->resourceId})\n";
+    }
+
+    function onClose(ConnectionInterface $conn)
+    {
+        $this->clientes->detach($conn);
+        echo "Cierre de Conexión => ({$conn->resourceId})\n";
+    }
+
+    function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "Error de Conexión => ({$conn->resourceId})\n";
+        $conn->close();
+    }
+
+    function onMessage(ConnectionInterface $from, $msg)
+    {
+        echo "{$from->resourceId} dice => {$msg}\n";
+    }
 }
